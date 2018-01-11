fd9e3d94abb79c0c6732487d0449428c18c3c270
Sylvain Viart <sylvain303@github>
2018-01-11 07:50:45 +0100

    test lib with only one chunk

--- python/test/remote_location/mylib.bash
+++ python/test/remote_location/mylib.bash
@@ -1,17 +1,4 @@
+# some_func
 some_func() {
     echo "param $1"
-}
-# some_func
-one() {
-    echo "code for one"
-}
-two() {
-    for i in $(seq 1 2)
-    do
-        one
-    done
-}
-three() {
-    echo "code for three"
-    two
 }
